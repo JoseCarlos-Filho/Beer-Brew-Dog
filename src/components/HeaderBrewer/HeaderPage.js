bcb4ad2d8387faeb60a064de4ffb8fa2ca7f263d
José Carlos <etijcvivi@gmail.com>
2022-08-20 00:00:35 -0300

    Projeto Cervejaria Brew Dog Beer - Teste de Hello word componente HeaderPage.

--- src/components/HeaderBrewer/HeaderPage.js
+++ src/components/HeaderBrewer/HeaderPage.js
@@ -1,9 +1,37 @@
 // Componente header da page - atalho para montar componente: rafce
 import React from "react";
-import "./Header.modulo.scss";
+import "./HeaderPage.scss";
+//import styles from "./HeaderPage.scss";
 
 const HeaderPage = () => {
-  return <div className="test">Hello Word José</div>;
+  return (
+    <div className="header">
+      <div className="header__content">
+        <div>
+          <span className="logo">Logo</span>
+        </div>
+
+        <div>
+          <nav className="navbar__header">
+            <a className="nav__item" href={"/"}>
+              Home
+            </a>
+            <a className="nav__item" href={"/"}>
+              Beers
+            </a>
+            <a className="nav__item" href={"/"}>
+              Contact
+            </a>
+          </nav>
+        </div>
+
+        <div>
+          <div className="header__login"></div>
+          <button className="user__login"></button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeaderPage;
